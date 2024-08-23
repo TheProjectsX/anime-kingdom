af6ffa9ff2f03a7127c2d5369556365f31fa0359
Modasser Hasan <manvstech2.0@gmail.com>
2024-08-24 01:24:28 +0600

    Anime Characters get created

--- server-side/utils/functions.py
+++ server-side/utils/functions.py
@@ -42,7 +42,6 @@ def getAnimeDetails(id):
     if (not data):
         return {"success": False, "message": serverResponse.get("message", "Item not Found")}, serverResponse.get("status", 404)
     
-    print(data)
     animeData = {
         "id": data.get("mal_id"),
         "title": data.get("title"),
@@ -79,8 +78,51 @@ def getAnimeDetails(id):
     return returnResponse, 200
 
 
-# 
+# Get Anime Characters
+def getAnimeCharacters(id):
+    path = f"/{id}/characters"
+    
+    serverResponse = animeBase(path)
+    if (not serverResponse["success"]): return serverResponse, 500
+    data = serverResponse.get("data")
+    
+    if (not data):
+        return {"success": False, "message": serverResponse.get("message", "Item not Found")}, serverResponse.get("status", 404)
 
+    returnResponse = {
+        "success": True,
+        "data": []
+    }
+    
+    for item in serverResponse["data"]:
+        character = item["character"]
+        voice_actors = item["voice_actors"]
+        
+        data = {
+            "id": character.get("mal_id", ""),
+            "name": character.get("name"),
+            "image": character.get("images", {}).get("jpg", {}).get("image_url"),
+            "role": item.get("role"),
+            "favorites": item.get("favorites"),
+            "voice_actors": []
+        }
+        
+        for actor in voice_actors:
+            person = actor["person"]
+            vaData = {
+                "id": person.get("mal_id", ""),
+                "name": person.get("name"),
+                "language": actor.get("language"),
+                "image": person.get("images", {}).get("jpg", {}).get("image_url"),
+            }
+            
+            data["voice_actors"].append(vaData)
+        
+        
+        returnResponse["data"].append(data)
+    
+    return returnResponse, 200
+    
 # Get Anime Data
 def getAnime(
         filters = {
