c14d57da6c3103d9089bd3e64f5e624779573f1b
Modasser Hasan <manvstech2.0@gmail.com>
2024-10-12 16:05:04 +0600

    Fixed cache using for banner (maybe)

--- client-side/src/app/page.jsx
+++ client-side/src/app/page.jsx
@@ -2,7 +2,9 @@ import AnimeHomePageItems from "@/components/anime/AnimeHomePageItems";
 import { loadServerData } from "@/utils/DataLoaderBeta";
 
 export default async function Home() {
-    const animeScheduleData = await loadServerData("/anime/schedule/24h");
+    const animeScheduleData = await loadServerData("/anime/schedule/24h", {
+        timestamp: Date.now(),
+    });
 
     return (
         <main className="max-width space-y-8 mb-10 pt-5">
