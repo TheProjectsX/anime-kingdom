5c36e05dc3bc730fdbc97cb8cf0b80a1c7d70210
Modasser Hasan <manvstech2.0@gmail.com>
2024-10-12 21:00:47 +0600

    Fixed cache issue

--- client-side/src/app/page.jsx
+++ client-side/src/app/page.jsx
@@ -2,9 +2,16 @@ import AnimeHomePageItems from "@/components/anime/AnimeHomePageItems";
 import { loadServerData } from "@/utils/DataLoaderBeta";
 
 export default async function Home() {
-    const animeScheduleData = await loadServerData("/anime/schedule/24h", {
-        timestamp: Date.now(),
-    });
+    const animeScheduleData = await loadServerData(
+        "/anime/schedule/24h",
+        {
+            timestamp: Date.now(),
+        },
+        {
+            method: "GET",
+            cache: "no-store",
+        }
+    );
 
     return (
         <main className="max-width space-y-8 mb-10 pt-5">
